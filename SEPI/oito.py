6cda60aabab4f694f7fca484d43914031e123a9f
LucasXMorais <lucas.xavier.de.morais@gmail.com>
2023-09-18 15:23:50 -0300

    Teste 8 com pequena alteração no código para desacoplado rápido

--- SEPI/oito.py
+++ SEPI/oito.py
@@ -1,5 +1,5 @@
-# 13/09/23 - Lucas Xavier de Morais
-# TESTE 7 
+# 18/09/23 - Lucas Xavier de Morais
+# TESTE 8 
 # SEP I - Engenharia Elétrica (UFSJ)
 import numpy as np
 
@@ -12,15 +12,31 @@ def matAdm(infoLinhas):
         k = int(linha[0]) - 1
         m = int(linha[1]) - 1
         admitancia = 1/(linha[2] + (1j*linha[3]))
-        susceptacancia = 1j*linha[4]/2
+        susceptancia = 1j*linha[4]/2
         tap = linha[5]
         defasagem = linha[6]*np.pi/180
-        matrizY[k][k] = matrizY[k][k] + (tap**2)*admitancia + susceptacancia
+        matrizY[k][k] = matrizY[k][k] + (tap**2)*admitancia + susceptancia
         matrizY[k][m] = -tap*np.exp(1j*defasagem)*admitancia
         matrizY[m][k] = -tap*np.exp(-1j*defasagem)*admitancia
-        matrizY[m][m] = matrizY[m][m] + admitancia + susceptacancia
+        matrizY[m][m] = matrizY[m][m] + admitancia + susceptancia
     
     return matrizY
+
+# Construção da matriz admitancia Y completa
+def matSusceptanciaIncompleta(infoLinhas):
+    Max = int(infoLinhas[np.argmax(infoLinhas,axis=0)[0]][np.argmax(infoLinhas,axis=1)[0]])
+    matrizBLinha = np.zeros((Max,Max))
+
+    for linha in infoLinhas: 
+        k = int(linha[0]) - 1
+        m = int(linha[1]) - 1
+        bkm = 1/linha[3]
+        matrizBLinha[k][k] = matrizBLinha[k][k] + bkm 
+        matrizBLinha[k][m] = -bkm
+        matrizBLinha[m][k] = -bkm
+        matrizBLinha[m][m] = matrizBLinha[m][m] + bkm 
+    
+    return matrizBLinha
 
 # Funcão para montar os vetores com todos os ângulos e tensões do sistema
 # para os angulos incógnitas o valor inicial é definido como 0 e para as tensões é 1
@@ -275,19 +291,45 @@ def montarJacobianoDesacoplado(matrizY, angulos, tensoes, indicesAngulos, indice
 
     return jacobiano
 
-# ----------------- TESTE 7 -----------------
+def fluxoPotencia(matrizY, angulos, tensoes, infoLinhas):
+    #fluxos = [barra de, barra para, fluxo de potencia ativa, fluxo de potencia reativa]
+    fluxos = np.zeros((len(angulos), 4))
+
+    c = 0
+    for linha in infoLinhas: 
+        k = int(linha[0]) - 1
+        m = int(linha[1]) - 1
+        gkm = (linha[2]) / (linha[2]**2 + linha[3]**2) 
+        bkm = (linha[3]) / (linha[2]**2 + linha[3]**2) 
+        bshunt = linha[4]/2 
+        tap = linha[5]
+        defasagem = linha[6]*np.pi/180
+        Vk = tensoes[k][0]
+        Vm = tensoes[m][0]
+        tapkmVk = tap * Vk 
+        thetakm = angulos[k][0] - angulos[m][0]
+        fluxos[c][0] = k + 1
+        fluxos[c][1] = m + 1
+        fluxos[c][2] = (((tapkmVk)**2)*gkm) - (tapkmVk*Vm*gkm*np.cos(thetakm + defasagem)) - (tapkmVk*Vm*bkm*np.sin(thetakm + defasagem))
+        fluxos[c][3] = (-((tapkmVk)**2)*(bkm + bshunt)) + (tapkmVk*Vm*bkm*np.cos(thetakm + defasagem)) - (tapkmVk*Vm*gkm*np.sin(thetakm + defasagem))
+        c += 1
+
+    return fluxos
+# ----------------- TESTE 8 -----------------
 
 # infoLinhas = [barra de, barra para, resistencia km, reatancia km, susceptancia shuntkm total, tapkm, defasagemkm]
 
-infoLinhas = np.array([ [1, 2, 0.006, 0.032, 0.010, 1.0, 0.0],
-                        [1, 3, 0.012, 0.064, 0.020, 1.0, 0.0],
-                        [2, 3, 0.036, 0.192, 0.060, 1.0, 0.0]])
+infoLinhas = np.array([ [1, 2, 0.00, 0.15, 0.0, 1.02, 0.0],
+                        [2, 3, 0.01, 0.10, 0.0, 1.00, 0.0],
+                        [2, 4, 0.02, 0.20, 0.0, 1.00, 0.0],
+                        [3, 4, 0.01, 0.10, 0.0, 1.00, 0.0]])
 
 # infoBarras = [barra , tipo, tensao, angulo, P.Ativa Geracao (PG), P.Reativa Geracao (QG), P.Ativa Carga (PL), P.Reativa Carga (QL) ]
 
-infoBarras = np.array([ [1, 'PV', 1.05, '-', 0.5, 0.0, 0.0, 0.0],
-                        [2, 'V0', 1.00, 0.0, 0.0, 0.0, 1.0, 0.3],
-                        [3, 'PQ', '-' , '-', 0.0, 0.0, 0.5, 0.2] ])
+infoBarras = np.array([ [1, 'V0', 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [2, 'PQ', '-', '-', 0.0, 0.0, 0.0, 0.0],
+                        [3, 'PV', 1.0, '-', 0.0, 0.0, 0.0, 0.0],
+                        [4, 'PQ', '-', '-', 0.0, 0.0, 1.0, 0.5] ])
 
 #Construcao da matriz admitania do sistema
 matrizY = np.copy(matAdm(infoLinhas))
@@ -308,10 +350,12 @@ indices, indicesAngulos, indicesTensoes = vetoresIndices(infoBarras, nPV, nPQ)
 #Calcular as potencias esperadas
 ativaEsp, reativaEsp = calcularPotenciasEsp(infoBarras)
 
+# print('Jacobiano Normal')
+print('Jacobiano Desacoplado')
 # Definindo parâmetros iniciais do método de newton
 maxIter = 100
 iter = 0
-tol = 0.03
+tol = 0.01
 fimP = 0
 fimQ = 0
 
@@ -358,17 +402,125 @@ while iter < maxIter :
 
     iter += 1
 
-# print('Jacobiano Normal')
-print('Jacobiano Desacoplado')
 print('Angulos finais')
 print(angulos)
 print('Tensoes finais')
 print(tensoes)
 
+potenciaAtivaFinal, potenciaReativaFinal = calcularPotenciasCalc(matrizY, angulos, tensoes)
+print('Potencias Finais')
+print('Potencia Ativa')
+print(potenciaAtivaFinal)
+print('Potencia Reativa')
+print(potenciaReativaFinal)
 
+# --- NEWTON DESACOPLADO RÁPDIO ---
+print('Jacobiano Desacoplado Rápido')
+#Construcao dos vetores de angulos e tensoes do sistema
+angulos, tensoes = vetoresAngulosTensoes(infoBarras)
+# Definindo parâmetros iniciais do método de newton desacoplado rápido
+maxIter = 100
+iter = 0
+tol = 0.01
+fimP = 0
+fimQ = 0
+BLinha = matSusceptanciaIncompleta(infoLinhas)
 
+while iter < maxIter :
+    # Calculando pcalc para valores iniciais
+    ativasKCalculada, reativasKCalculada = calcularPotenciasCalc(matrizY, angulos, tensoes)
 
+    #Calculando deltaP e deltaQ
+    deltaPks = np.zeros((nPV+nPQ,1))
+    deltaQks = np.zeros((nPQ,1))
+    c = 0
+    for i in indicesAngulos:
+        deltaPks[c] = ativaEsp[i-1] + ativasKCalculada[i-1]
+        c += 1
+    c = 0
+    for i in indicesTensoes:
+        deltaQks[c] = reativaEsp[i-1] + reativasKCalculada[i-1]
+        c += 1
 
+    if np.max(np.abs(deltaPks)) < tol: 
+        fimP = 1
+    else: 
+        fimP = 0
+
+        #Montando o jacobiano da iteração
+        HLinha = np.zeros((nPV+nPQ, nPV+nPQ))
+        for linha in range(nPV+nPQ):
+            # Indice atual é o indice do angulo - 1 para poder iterar sorbe a array
+            k = indicesAngulos[linha] - 1
+            for coluna in range(nPV+nPQ):
+                m = indicesAngulos[coluna] - 1
+                Bkm = BLinha[k][m]
+                HLinha[linha][coluna] = Bkm 
+
+        # Fazendo deltaP / V
+        deltaPporV = np.zeros(np.shape(deltaPks))
+        for i in range(len(indicesAngulos)):
+            deltaPporV[i] = deltaPks[i] / tensoes[indicesAngulos[i]-1]
+
+        # Fazendo d0 = -HLinha^(-1) * (deltaP/V)
+        deltaTheta = np.zeros((len(indicesAngulos),1))
+        deltaTheta = (-1)*np.dot(np.linalg.inv(HLinha), deltaPporV) 
+
+        # Atualizando os ângulos
+        c = 0
+        for a in indicesAngulos:
+            angulos[a-1] += deltaTheta[c]
+            c += 1
+
+    if np.max(np.abs(deltaQks)) < tol: 
+        fimQ = 1
+    else: 
+        fimQ = 0
+
+        #Montando o jacobiano da iteração
+        LLinha = np.zeros((    nPQ,     nPQ))
+        for linha in range(nPQ):
+            # Indice atual é o indice do angulo - 1 para poder iterar sorbe a array
+            k = indicesAngulos[linha] - 1
+            for coluna in range(nPQ):
+                m = indicesAngulos[coluna] - 1
+                Bkm = matrizY.imag[k][m]
+                LLinha[linha][coluna] = -Bkm 
+
+        # Fazendo deltaP / V
+        deltaQporV = np.zeros(np.shape(deltaQks))
+        for i in range(len(indicesTensoes)):
+            deltaQporV[i] = deltaQks[i] / tensoes[indicesAngulos[i]-1]
+
+        # Fazendo dV = -LLinha^(-1) * (deltaQ/V)
+        deltaV = np.zeros((len(indicesAngulos),1))
+        deltaV = (-1)*np.dot(np.linalg.inv(LLinha), deltaQporV) 
+
+        # Atualizando as tensões 
+        c = 0
+        for t in indicesTensoes:
+            tensoes[t-1] += deltaV[c]
+            c += 1
+
+    iter += 1
+
+    if fimP and fimQ: print('Convergiu em ', iter, ' iterações'); break
+
+print('Angulos finais')
+print(angulos)
+print('Tensoes finais')
+print(tensoes)
+
+potenciaAtivaFinal, potenciaReativaFinal = calcularPotenciasCalc(matrizY, angulos, tensoes)
+print('Potencias Finais')
+print('Potencia Ativa')
+print(potenciaAtivaFinal)
+print('Potencia Reativa')
+print(potenciaReativaFinal)
+
+print('Fluxos de Potências Ativa e Reativa')
+fluxos = np.copy(fluxoPotencia(matrizY, angulos, tensoes, infoLinhas))
+print(fluxos)
 
 
 
