22e43bece9f5e828754263b49bbe7c71eec70a8c
Lucas Morais <lucas.xavier.de.morais@gmail.com>
2023-11-13 13:05:24 -0300

    analise fa-fa-te

--- SEPI/fase-fase-terra.py
+++ SEPI/fase-fase-terra.py
@@ -1,5 +1,5 @@
 # 13/11/23 - Lucas Xavier de Morais
-# TESTE 14 
+# Análise de falta fase-fase-terra
 # SEP I - Engenharia Elétrica (UFSJ)
 import numpy as np
 
@@ -28,10 +28,6 @@ def matAdm(infoLinhas):
     
     return matrizY
 
-# Função criada para invereter uma matriz singular quadrada com a função lstsq do numpy
-# Como nessa materia estao sendo usadas matrizes complexas, uso a parte imaginaria que 
-# e o que importa pro exercicio. Mas funciona com a real. O que muda e que o resultado fica 
-# mais redondo. 
 def inversaEspecial(m):
     a, b = m.shape
     if a != b: raise ValueError("A funcao inversa especial funciona apenas com matrizes quadradas")
@@ -46,8 +42,18 @@ tranformacaoT = np.array( [ [1,      1,      1],
                             [1, alpha2,  alpha],
                             [1,  alpha, alpha2 ]])
 
-# ----------------- TESTE 14 -----------------
+# ----------------- ROTINA -----------------
 
+# Parâmetros do exercício
+
+# Definindo a barra do curto 
+barraCurto = 5
+
+# Definicao da fases do curto fase-fase
+# 1 : falta AB; 2 : falta BC; 3 : falta AC
+falta = 2
+
+# Construir aqui as tabelas para as linhas para cada sequencia
 # infoLinhas = [barra de, barra para, resistencia km, reatancia km, susceptancia shuntkm total, tapkm, defasagemkm]
 
 infoLinhasPos = np.array([  [1, 1, 0.0, 0.20, 0.0, 1.0, 0.0],
@@ -73,18 +79,19 @@ infoLinhasZero = np.array([ [1, 1, 0.0, 0.05, 0.0, 1.0, 0.0],
                             [6, 6, 0.0, 0.20, 0.0, 1.0, 0.0],
                             [5, 5, 0.0, 0.05, 0.0, 1.0, 0.0]])
 
+
 #Construção das matrizes positiva
 matrizYPos = np.copy(matAdm(infoLinhasPos))
 impedanciaPos = np.copy(inversaEspecial(matrizYPos))
 print("Matrizes sequencia positiva")
-# print(matrizYPos)
+print(matrizYPos)
 print(impedanciaPos.imag)
 
 #Construção das matrizes negativa
 matrizYNeg = np.copy(matAdm(infoLinhasNeg))
 impedanciaNeg = np.copy(inversaEspecial(matrizYNeg))
 print("Matrizes sequencia negativa")
-# print(matrizYNeg)
+print(matrizYNeg)
 print(impedanciaNeg.imag)
 
 
@@ -92,15 +99,16 @@ print(impedanciaNeg.imag)
 matrizYZero = np.copy(matAdm(infoLinhasZero))
 impedanciaZero = np.copy(inversaEspecial(matrizYZero))
 print("Matrizes sequencia zero")
-# print(matrizYZero.imag)
+print(matrizYZero.imag)
 print(impedanciaZero.imag)
 
-#para uma falta na barra 5, utiliza-se os valores da impedacnia em 5,5
-ZPos  = impedanciaPos[4][4]
-ZNeg  = impedanciaNeg[4][4]
-ZZero = impedanciaZero[4][4]
+# Impedancias da barra de falta
+barra = barraCurto - 1
+ZPos  = impedanciaPos[barra][barra]
+ZNeg  = impedanciaNeg[barra][barra]
+ZZero = impedanciaZero[barra][barra]
 
-#Para as correntes de falta na barra 5 considerando pre-falta como 1/_0
+# Para as correntes de falta na barra de curto considerando pre-falta como 1/_0
 ZEq = ZPos + ( (ZNeg*ZZero) / (ZNeg+ZZero) )
 correntePos = 1 / ZEq
 tensaoPos = 1 - (ZPos * correntePos)
@@ -113,26 +121,38 @@ correnteZero = -tensaoZero / ZZero
 correntesComponentesComplexas = np.array( [ [correnteZero], [correntePos], [correnteNeg] ] )
 correntesDesequilibradasPonto = np.dot( tranformacaoT, correntesComponentesComplexas )
 
-vetorAuxCorrente = np.array([ [0.0], [0.0], [0.0], [0.0], [-correnteZero], [0.0] ])
-vetorAuxTensao = np.array([ [0.0], [0.0], [0.0], [0.0], [0.0], [0.0] ])
+# Calculo das tensoes nas barras
+numeroBarras = matrizYPos.shape[0]
+
+vetorAuxCorrente = np.zeros((numeroBarras,1),dtype=np.complex_)
+vetorAuxCorrente[barra] = -correnteZero
+vetorAuxTensao = np.zeros((numeroBarras,1))
 tensoesBarrasZero = vetorAuxTensao + np.dot(impedanciaZero, vetorAuxCorrente)
 
-vetorAuxCorrente = np.array([ [0.0], [0.0], [0.0], [0.0], [-correntePos], [0.0] ])
-vetorAuxTensao = np.array([ [1.0], [1.0], [1.0], [1.0], [1.0], [1.0] ])
+vetorAuxCorrente = np.zeros((numeroBarras,1),dtype=np.complex_)
+vetorAuxCorrente[barra] = -correntePos
+vetorAuxTensao = np.ones((numeroBarras,1))
 tensoesBarrasPos = vetorAuxTensao + np.dot(impedanciaPos, vetorAuxCorrente)
 
-vetorAuxCorrente = np.array([ [0.0], [0.0], [0.0], [0.0], [-correnteNeg], [0.0] ])
-vetorAuxTensao = np.array([ [0.0], [0.0], [0.0], [0.0], [0.0], [0.0] ])
+vetorAuxCorrente = np.zeros((numeroBarras,1),dtype=np.complex_)
+vetorAuxCorrente[barra] = -correnteNeg
+vetorAuxTensao = np.zeros((numeroBarras,1))
 tensoesBarrasNeg = vetorAuxTensao + np.dot(impedanciaNeg, vetorAuxCorrente)
 
+# Calculo das tensoes em todas as barras
 tensoesDesequilibradas = []
-for barra in range(6):
+for barra in range(numeroBarras):
     compZero = tensoesBarrasZero.real[0][0]
     compPos = tensoesBarrasPos.real[0][0]
     compNeg = tensoesBarrasNeg.real[0][0]
     vetorAux = np.array([ [compZero], [compPos], [compNeg] ])
     tensoesDesequilibradas.append(np.dot( tranformacaoT, vetorAux ))
 
+# ---------- FIM ANALISE FASE-FASE-TERRA ----------
+# Daqui pra baixo e feita a análise do exercicio
+# No exercicio abaixo, o objeitvo era calcular as coorrentes nos 
+# geradores localizados nas barras 1 e 5
+# A analise acima e a analise base
 # Os geradores 1 e 2 estao nas barras 1 e 5 respectivamente
 
 #Gerador 1
